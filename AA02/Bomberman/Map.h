93bfdaf807903ad7b60139c1f351fd0f91e4543c
David <david.saiz@enti.cat>
2020-03-27 18:57:24 +0100

    Lectura mapa

--- AA02/Bomberman/Map.h
+++ AA02/Bomberman/Map.h
@@ -1,9 +1,68 @@
 #pragma once
 #include <iostream>
-#include <vector>
-class map {
+#include <fstream>
+#include "Types.h"
+
+
+class Map {
 private:
-	std::vector<>map;
+	char **map;
+	int numRows, numColumns;
 
 public:
+	//CONSTRUCTOR
+	Map() {
+		
+		std::ifstream myFile("config.txt");
+		
+
+		if (myFile.is_open())
+		{
+			if (!myFile.eof())
+			{
+				myFile >> numRows;
+				myFile.ignore();
+				myFile >> numColumns;
+
+				map = new char*[numRows];
+
+				for (int i = 0; i < numRows; i++) {
+					map[i] = new char[numColumns];
+				}
+
+				myFile.ignore();
+
+					for (int i = 0; i < numRows; i++) {
+						for (int j = 0; j < numColumns; j++) {
+							myFile.get(map[i][j]) >> std::noskipws;
+						}
+						myFile.ignore();
+					}
+				
+			}
+			myFile.close();
+		}
+		else std::cout << "Unable to open file";
+		
+		
+		
+
+		
+
+		
+
+
+	}
+
+	//FUNCIONES
+	void print() {
+		for (int i = 0; i < numRows; i++) {
+			for (int j = 0; j < numColumns; j++) {
+				std::cout << map[i][j];
+			}
+			std::cout << std::endl;
+		}
+	}
+
+
 };
--- AA02/Bomberman/Source.cpp
+++ AA02/Bomberman/Source.cpp
@@ -1,5 +1,11 @@
 #include "Player.h"
+#include "Map.h"
 int main() {
-	
+
+	Map map;
+
+	map.print();
+
+	return 0;
 
 }
